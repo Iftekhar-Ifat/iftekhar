cfa5cc6a45214e2a5c08e2913d3d70c1a87c2656
Iftekhar Ahmed <iftekharifat007@gmail.com>
2022-08-05 22:42:57 +0600

    changed navbar button function

--- components/NavBar.js
+++ components/NavBar.js
@@ -31,6 +31,14 @@ const NavBar = () => {
 
     const route = useRouter();
 
+    const navButtonHandler = (e) => {
+        let buttonPressed = e.target.outerText.toLowerCase();
+        if (buttonPressed === "home") {
+            route.push("/");
+        } else {
+            route.push(`/${buttonPressed}`);
+        }
+    };
     return (
         <div
             className="sticky top-0 z-10 "
@@ -73,28 +81,30 @@ const NavBar = () => {
                                     Home
                                 </button>
                             </Link>
-                            <Link href="projects">
-                                <button
-                                    className={`ml-8 inline-flex items-center justify-center px-4 py-2 rounded-md font-medium text-white hover:bg-backgroundGray ${
-                                        route.pathname === "/projects"
-                                            ? "bg-backgroundGray"
-                                            : "bg-black"
-                                    }`}
-                                >
-                                    Projects
-                                </button>
-                            </Link>
-                            <Link href="blogs">
-                                <button
-                                    className={`ml-8 inline-flex items-center justify-center px-4 py-2 rounded-md font-medium text-white hover:bg-backgroundGray ${
-                                        route.pathname === "/blogs"
-                                            ? "bg-backgroundGray"
-                                            : "bg-black"
-                                    }`}
-                                >
-                                    Blogs
-                                </button>
-                            </Link>
+                            {/* <Link href="projects"> */}
+                            <button
+                                className={`ml-8 inline-flex items-center justify-center px-4 py-2 rounded-md font-medium text-white hover:bg-backgroundGray ${
+                                    route.pathname === "/projects"
+                                        ? "bg-backgroundGray"
+                                        : "bg-black"
+                                }`}
+                                onClick={navButtonHandler}
+                            >
+                                Projects
+                            </button>
+                            {/* </Link> */}
+                            {/* <Link href="blogs"> */}
+                            <button
+                                className={`ml-8 inline-flex items-center justify-center px-4 py-2 rounded-md font-medium text-white hover:bg-backgroundGray ${
+                                    route.pathname === "/blogs"
+                                        ? "bg-backgroundGray"
+                                        : "bg-black"
+                                }`}
+                                onClick={navButtonHandler}
+                            >
+                                Blogs
+                            </button>
+                            {/* </Link> */}
                         </div>
                     </div>
                 </div>
@@ -136,20 +146,19 @@ const NavBar = () => {
                                 <div className="mt-10">
                                     <nav className="grid gap-y-8">
                                         {solutions.map((item) => (
-                                            <Link
-                                                href={item.href}
+                                            <a
+                                                className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50"
+                                                onClick={navButtonHandler}
                                                 key={item.name}
                                             >
-                                                <a className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50">
-                                                    <item.icon
-                                                        className="flex-shrink-0 h-6 w-6 text-indigo-600"
-                                                        aria-hidden="true"
-                                                    />
-                                                    <span className="ml-3 text-base font-medium text-gray-900">
-                                                        {item.name}
-                                                    </span>
-                                                </a>
-                                            </Link>
+                                                <item.icon
+                                                    className="flex-shrink-0 h-6 w-6 text-indigo-600"
+                                                    aria-hidden="true"
+                                                />
+                                                <span className="ml-3 text-base font-medium text-gray-900">
+                                                    {item.name}
+                                                </span>
+                                            </a>
                                         ))}
                                     </nav>
                                 </div>

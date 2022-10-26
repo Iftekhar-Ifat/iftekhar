c245a55e5ee96483699615255f3c132c8a3ded7d
Iftekhar Ahmed <iftekharifat007@gmail.com>
2022-10-26 23:07:51 +0600

    isr check 2

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -3,38 +3,44 @@ import { isValidSignature, SIGNATURE_HEADER_NAME } from "@sanity/webhook";
 const SANITY_WEBHOOK_SECRET = process.env.SANITY_WEBHOOK_SECRET;
 
 export default async function handler(req, res) {
-    const signature = req.headers[SIGNATURE_HEADER_NAME].toString();
-    const isValid = isValidSignature(
-        JSON.stringify(req.body),
-        signature,
-        SANITY_WEBHOOK_SECRET
-    );
-
-    console.log(`===== Is the webhook request valid? ${isValid}`);
-
-    // Validate signature
-    if (!isValid) {
-        return res
-            .status(401)
-            .json({ success: false, message: "Invalid signature" });
-    } else {
-        try {
-            const pathToRevalidate = req.body.slug.current;
-
-            console.log(`===== Revalidating: ${req.body.slug}`);
-            console.log(req.body._type);
-
-            if (req.body._type === "post") {
-                await res.revalidate(`/blogs/${pathToRevalidate}`);
-            } else if (req.body._type === "projects") {
-                await res.revalidate(`/projects/${pathToRevalidate}`);
-            }
-
-            return res.json({ revalidated: true });
-        } catch (err) {
-            // Could not revalidate. The stale page will continue to be shown until
-            // this issue is fixed.
-            return res.status(500).send("Error while revalidating");
-        }
+    const signature = req.headers[SIGNATURE_HEADER_NAME];
+    const body = await readBody(req);
+    if (!isValidSignature(body, signature, SANITY_WEBHOOK_SECRET)) {
+        res.status(401).json({ success: false, message: "Invalid signature" });
+        return;
     }
+
+    const jsonBody = JSON.parse(body);
+    try {
+        const pathToRevalidate = jsonBody.slug.current;
+
+        console.log(`===== Revalidating: ${jsonBody.slug}`);
+        console.log(jsonBody._type);
+
+        if (jsonBody._type === "post") {
+            await res.revalidate(`/blogs/${pathToRevalidate}`);
+        } else if (jsonBody._type === "projects") {
+            await res.revalidate(`/projects/${pathToRevalidate}`);
+        }
+
+        return res.json({ revalidated: true });
+    } catch (err) {
+        // Could not revalidate. The stale page will continue to be shown until
+        // this issue is fixed.
+        return res.status(500).send("Error while revalidating");
+    }
+}
+
+export const config = {
+    api: {
+        bodyParser: false,
+    },
+};
+
+async function readBody(readable) {
+    const chunks = [];
+    for await (const chunk of readable) {
+        chunks.push(typeof chunk === "string" ? Buffer.from(chunk) : chunk);
+    }
+    return Buffer.concat(chunks).toString("utf8");
 }

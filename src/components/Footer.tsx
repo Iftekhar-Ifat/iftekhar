947aa3e61f5ed6410ed559517d6056f2c983665a
Iftekhar-Ifat <iftekharifat007@gmail.com>
2024-02-27 01:59:00 +0600

    updated footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="md:px-[16%]">
       <div className="hr_line"></div>
-      <div className="flex md:text-base justify-between text-muted-foreground mb-8 px-2">
+      <div className="flex md:text-base justify-between text-muted-foreground mb-8 px-2 md:px-0">
         <p className="text-base py-0">
           Built with{" "}
           <a
